ca7f33fc0c2eff283cde0b98686241ea2b7f8be5
byeongkookkang <byeongguk1998@gmail.com>
2026-01-03 18:38:44 +0900

    chore: 프로젝트 초기 에셋 정리 및 패키지 설정 업데이트
    - 불필요한 Next.js 기본 아이콘(svg) 삭제
    - 캐러셀 및 상품용 이미지 에셋 추가
    - package.json: bcrypt, lucide-react 등 의존성 추가
    -fakestoreapi.com로 부터 수신하는 이미지 사용 허가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/**', // 해당 도메인의 모든 경로 허용
+      },
+    ],
+  },
 };
 
 export default nextConfig;
